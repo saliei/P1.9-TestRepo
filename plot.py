f912efe08897020c23bef39fff9d903af56fbbce
saliei <saeidaliei2019@gmail.com>
2020-02-10 15:55:49 +0100

    add usage help

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from __future__ import print_function
 
 plt.style.use('ggplot')
 plt.rc('font', family='monospace')
@@ -10,6 +11,10 @@ start = -5
 stop = -start
 # include stop
 xval = np.arange(start, stop+step, step)
+
+if len(sys.argv) < 2:
+    print("Usage: plot.py <func-number>")
+    print("\t <func-number>: 1: f(x)=x")
 funcNum = int(sys.argv[1])
 
 def f1(x):
