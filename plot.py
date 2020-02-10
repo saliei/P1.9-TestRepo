6dea9edbc0161e9a273b6afcf4570c57a9cf794d
saliei <saeidaliei2019@gmail.com>
2020-02-10 16:05:34 +0100

    update switcher for f2 & f3

--- plot.py
+++ plot.py
@@ -30,7 +30,9 @@ def f3(x):
 
 def runFunc(i):
     return{
-            1: f1(xval)
+            1: f1(xval),
+            2: f2(xval),
+            3: f3(xval),
             }[i]
 
 yval = runFunc(funcNum)
